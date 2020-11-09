bb6cbbfe7ef9592937864a94593ff96fb1eb464f
nik-1207 <nikhil.mohan_cs18@gla.ac.in>
2020-11-09 23:16:29 +0530

    android samples for food apps

---
+++ app/src/main/java/com/example/sample/DetailsActivity.java
@@ -0,0 +1,18 @@
+package com.example.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
